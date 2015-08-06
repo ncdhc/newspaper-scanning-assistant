64c4d64ad84bc84c5d8c4b51e4bc166bb5520b30
Stephanie Williams <sadamson@email.unc.edu>
2015-08-06 11:45:28 -0400

    error logic fixes

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>PaperBoy!</title>
+    <title>Scanning Assistant</title>
 
     <!-- Bootstrap -->
     <link href="css/bootstrap.darkly.min.css" rel="stylesheet">
@@ -20,7 +20,7 @@
   <body>
     <div class="container-fluid">
       <div class="row">
-      <h1>PaperBoy!</h1>
+      <h1>Scanning Assistant</h1>
       <hr>
       </div>
       <div class="row">
@@ -85,13 +85,25 @@
     <!-- Include all compiled plugins (below), or include individual files as needed -->
     <script src="js/bootstrap.min.js"></script>
     <script type="text/javascript">
-    $('.batchinput').keyup(function() {
+    
+    $('#batchname').keyup(function() {
             $('.error-keyup').remove();
             $('#startbutton').removeAttr('disabled');
             var inputVal = $(this).val();
             var characterReg = /^[a-zA-Z0-9]+$/;
             if (!characterReg.test(inputVal)) {
-                $('#startbatch').before('<p class="text-danger error-keyup"><span class="glyphicon glyphicon-exclamation-sign"></span> No spaces or special characters allowed.</p>');
+                $('#batchname').before('<p class="text-danger error-keyup"><span class="glyphicon glyphicon-exclamation-sign"></span> No spaces or special characters allowed.</p>');
+                $('#startbutton').attr('disabled','disabled');
+            }
+        });
+        
+        $('#batchcreator').keyup(function() {
+            $('.error-keyup').remove();
+            $('#startbutton').removeAttr('disabled');
+            var inputVal = $(this).val();
+            var characterReg = /^[a-zA-Z0-9]+$/;
+            if (!characterReg.test(inputVal)) {
+                $('#batchcreator').before('<p class="text-danger error-keyup"><span class="glyphicon glyphicon-exclamation-sign"></span> No spaces or special characters allowed.</p>');
                 $('#startbutton').attr('disabled','disabled');
             }
         });
--- scan.php
+++ scan.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>PaperBoy! &middot; Scan Batch</title>
+        <title>Scanning Assistant &middot; Scan Batch</title>
 
         <!-- Bootstrap -->
         <link href="css/bootstrap.darkly.min.css" rel="stylesheet">
